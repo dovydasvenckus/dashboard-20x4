cf55ee1ec066fd8ec4cf2416f468621ac4e5afff
Dovydas Venckus <dovydas.venckus@live.com>
2020-04-15 21:16:34 +0100

    Display positive temperature with plus sign

--- temperature.py
+++ temperature.py
@@ -7,6 +7,11 @@ def get_temperature():
     try:
         page = requests.get(url, timeout=10)
         tree = html.fromstring(page.content)
-        return tree.xpath('//div[@class="weather_info type_1"]//span[@class="temperature"]/text()')[0]
+        temperature=int(tree.xpath('//div[@class="weather_info type_1"]//span[@class="temperature"]/text()')[0])
+
+        if temperature > 0:
+            return '+' + str(temperature)
+        else:
+            return temperature
     except requests.exceptions.ConnectionError:
         return None
